534b5cb9e0321c5a394e1a846c9c152e4bf5ecdf
Varindakhanna <75711546+Varindakhanna@users.noreply.github.com>
2021-05-11 23:11:12 +0530

    Add files via upload

---
+++ a.php
@@ -0,0 +1,18 @@
+<?php
+
+$server = "localhost";
+
+    $username = "root";
+
+    $password = "";
+
+
+    $con = mysqli_connect($server, $username, $password);
+
+
+if(!$con)
+{
+    echo "Success connecting to the db";
+}
+
+?>
